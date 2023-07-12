30009aed601f6355e529fbffe93e203f1782e000
IzIBrizI007 <anatolij.goncharenko90@gmail.com>
2023-07-12 10:44:37 +0300

    add many blocks

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
 $(function () {
+
+  $('.team__card').on('click', function(){
+  $('.team__card').removeClass('team__card--active');
+  $(this).addClass('team__card--active');
+  });
+
+  $('.questions__item').on('click', function(){
+  $('.questions__item').removeClass('questions__item--active'); 
+  $(this).addClass('questions__item--active');
+  });
   
 });
+
+
+
+
+
+
